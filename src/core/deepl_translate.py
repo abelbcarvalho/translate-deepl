60ae90ad19a253b5e35354a8ad308eff5d294df5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-23 20:06:28 -0300

    feat: core translator created

---
+++ src/core/deepl_translate.py
@@ -0,0 +1,23 @@
+from os import environ
+from typing import List
+
+from deepl import Translator, TextResult
+
+from dotenv import load_dotenv
+
+from src.models.translate import Translate
+
+load_dotenv()
+
+
+class DeepLTranslate:
+    translator: Translator
+
+    def __init__(self):
+        auth_key = environ["DEEPL_AUTH_KEY"]
+        self.translator = Translator(auth_key)
+
+    async def translate(self, translate: Translate) -> TextResult | List[TextResult]:
+        text_translated = self.translator.translate_text(**translate.model_dump())
+
+        return text_translated
