465fec570cb0b874a3436de90ad151ff03a66b69
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-24 20:17:48 -0300

    fix: translate generic model working well

--- src/models/translate.py
+++ src/models/translate.py
@@ -1,23 +1,21 @@
 from typing import Optional, List
 
+from deepl import Language
 from pydantic import BaseModel
-
-from src.enums.enum_target_lang import EnumTargetLang
-from src.enums.enum_source_lang import EnumSourceLang
 
 
 class Translate(BaseModel):
     text: str
-    target_lang: EnumTargetLang
-    source_lang: Optional[EnumSourceLang] = None
+    target_lang: Language
+    source_lang: Optional[Language] = None
     context: Optional[str] = None
     split_sentences: Optional[str] = None
     preserve_formatting: Optional[bool] = None
     formality: Optional[str] = None
-    glossary_id: Optional[str] = None
-    show_billed_characters: Optional[bool] = None
     tag_handling: Optional[str] = None
     outline_detection: Optional[bool] = None
     non_splitting_tags: Optional[List[str]] = None
     splitting_tags: Optional[List[str]] = None
     ignore_tags: Optional[List[str]] = None
+
+    model_config = dict(arbitrary_types_allowed=True)
