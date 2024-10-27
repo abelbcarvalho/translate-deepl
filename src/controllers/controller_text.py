2cf535d76245e82f66034a8d3b1c5f33903d5d0e
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-27 11:50:51 -0300

    feat: route for text declared and controller basic structure

---
+++ src/controllers/controller_text.py
@@ -0,0 +1,6 @@
+class ControllerText():
+    def __init__(self):
+        pass
+
+    async def translate_text(self, text: any):
+        pass
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request
 
+from src.controllers.controller_text import ControllerText
 from src.controllers.controller_word import ControllerWord
 
 
 deepl = Blueprint("deepl", __name__)
 
 controller_word = ControllerWord()
+controller_text = ControllerText()
 
 
 @deepl.route("/", methods=["GET"])
@@ -13,8 +15,21 @@ def index():
     return {"hello": "world"}
 
 
+# word model routes
+
+
 @deepl.route("/translate/word", methods=["POST"])
 async def translate_word():
     body = request.json
 
     return await controller_word.translate_word(body)
+
+
+# text model routes
+
+
+@deepl.route("/translate/text", methods=["POST"])
+async def translate_text():
+    body = request.json
+
+    return await controller_text.translate_text(body)
